cb8c1f34c716cc3e37980515c7d0509a31e7ed08
Bình Thắng Võ <vobinhthang123@gmail.com>
2024-01-10 15:59:44 +0700

    Add project files.

---
+++ api_booking_hotel/DBContext/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api_booking_hotel.DBContext
+{
+    public class MyDbContext : DbContext
+    {
+
+        public MyDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
